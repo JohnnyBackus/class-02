757a99617da8c5dc8b32187b88192c057f16f94d
JohnnyBackus <john.p.backus@gmail.com>
2023-10-24 13:08:13 -0700

    added config files, added js file, added html file, added styles folder and css file

---
+++ app.js
@@ -0,0 +1,10 @@
+console.log('hello world');
+
+let userName = prompt('Hello there, and welcome to my website! We\'re going to play a little guessing game before we load this page. But first, please tell me your name.');
+console.log(userName);
+
+// need five yes/no questions that accept either y/n or yes/no responses. normalize the input with .toLowerCase() method.
+
+let sample = prompt(`Nice to meet you, ${userName}! Question goes here.`);
+console.log(sample);
+
