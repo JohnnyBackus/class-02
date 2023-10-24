16d6c1c2f26eef966e393f518ba904f6de2d2a9b
JohnnyBackus <john.p.backus@gmail.com>
2023-10-24 13:55:08 -0700

    added gitignore and markdown.json files, began work on user prompts in JS file, added Bio section to HTML file

--- app.js
+++ app.js
@@ -1,10 +1,67 @@
 console.log('hello world');
 
-let userName = prompt('Hello there, and welcome to my website! We\'re going to play a little guessing game before we load this page. But first, please tell me your name.');
+let userName = prompt('Hello there, and welcome to my website! We\'re going to play a little game of two truths and a lie (or in this case, three truths and two lies) before we load this page. But first, please tell me your name.');
 console.log(userName);
-
-// need five yes/no questions that accept either y/n or yes/no responses. normalize the input with .toLowerCase() method.
 
 let sample = prompt(`Nice to meet you, ${userName}! Question goes here.`);
 console.log(sample);
+let normalizedSample = sample.toLowerCase();
 
+if (normalizedSample === 'yes' || normalizedSample === 'y') {
+  // console.log('You guessed correct!');
+  alert('You guessed correct!')
+} else if (normalizedSample === 'no' || normalizedSample === 'n') {
+  // console.log('Sorry, you guessed incorrectly.');
+  alert('Sorry, you guessed incorrectly.');
+} else {
+  // console.log('Sorry, I don\'t recognize that response. Next time, please only answer yes or no.');
+  alert('Sorry, I don\'t recognize that response. Next time, please only answer yes or no.')
+}
+
+let sample = prompt(`Nice to meet you, ${userName}! Question goes here.`);
+console.log(sample);
+let normalizedSample = sample.toLowerCase();
+
+if (normalizedSample === 'yes' || normalizedSample === 'y') {
+  console.log('You guessed correct!');
+} else if (normalizedSample === 'no' || normalizedSample === 'n') {
+  console.log('Sorry, you guessed incorrectly.');
+} else {
+  console.log('Sorry, I don\'t recognize that response. Next time, please only answer yes or no.');
+}
+
+let sample = prompt(`Nice to meet you, ${userName}! Question goes here.`);
+console.log(sample);
+let normalizedSample = sample.toLowerCase();
+
+if (normalizedSample === 'yes' || normalizedSample === 'y') {
+  console.log('You guessed correct!');
+} else if (normalizedSample === 'no' || normalizedSample === 'n') {
+  console.log('Sorry, you guessed incorrectly.');
+} else {
+  console.log('Sorry, I don\'t recognize that response. Next time, please only answer yes or no.');
+}
+
+let sample = prompt(`Nice to meet you, ${userName}! Question goes here.`);
+console.log(sample);
+let normalizedSample = sample.toLowerCase();
+
+if (normalizedSample === 'yes' || normalizedSample === 'y') {
+  console.log('You guessed correct!');
+} else if (normalizedSample === 'no' || normalizedSample === 'n') {
+  console.log('Sorry, you guessed incorrectly.');
+} else {
+  console.log('Sorry, I don\'t recognize that response. Next time, please only answer yes or no.');
+}
+
+let sample = prompt(`Nice to meet you, ${userName}! Question goes here.`);
+console.log(sample);
+let normalizedSample = sample.toLowerCase();
+
+if (normalizedSample === 'yes' || normalizedSample === 'y') {
+  console.log('You guessed correct!');
+} else if (normalizedSample === 'no' || normalizedSample === 'n') {
+  console.log('Sorry, you guessed incorrectly.');
+} else {
+  console.log('Sorry, I don\'t recognize that response. Next time, please only answer yes or no.');
+}
